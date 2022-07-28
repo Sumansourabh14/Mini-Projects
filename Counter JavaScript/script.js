8dc1ce89e79a68fcc3ba4aca8b0d7504338cc68d
Suman Sourabh <71708709+Sumansourabh14@users.noreply.github.com>
2022-07-28 21:01:07 +0530

    Added two changes

--- Counter JavaScript/script.js
+++ Counter JavaScript/script.js
@@ -3,19 +3,18 @@ const resetBtn = document.querySelector('.reset-btn');
 const increaseBtn = document.querySelector('.increase-btn');
 let countNumber = document.querySelector('.count-number');
 
+//Starting value of counter
 let counter = 0;
 
 decreaseBtn.addEventListener('click', function decCounter() {
     countNumber.textContent = --counter;
-    console.log(counter);
 })
 
 resetBtn.addEventListener('click', function resetCounter() {
+    counter = 0;
     countNumber.textContent = counter;
 })
 
 increaseBtn.addEventListener('click', function incCounter() {
     countNumber.textContent = ++counter;
-    console.log(counter);
 })
-
