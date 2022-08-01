bf0aea17a42bc28f0afcd8475aa4446b917ffa6a
Suman Sourabh <71708709+Sumansourabh14@users.noreply.github.com>
2022-08-01 19:11:44 +0530

    Added one project

---
+++ Currency Converter - INR to etc/script.js
@@ -0,0 +1,32 @@
+const amount = document.querySelector('.input-value');
+const currencies = document.querySelector('.from-currency');
+const convertBtn = document.querySelector('.convert-btn');
+
+const output = document.querySelector('.output');
+
+convertBtn.addEventListener('click', function convert() {
+    output.innerHTML = "";
+
+    let singleOutput = document.createElement('div');
+    singleOutput.innerHTML = convertFromCurrency();
+    output.appendChild(singleOutput);
+});
+
+function convertFromCurrency() {
+    switch (currencies.selectedIndex) {
+        case 0:
+            return convertUSD();
+        case 1:
+            return convertEUR();
+        default:
+            break;
+    }
+}
+
+function convertUSD() {
+    return amount.value * 0.013;
+}
+
+function convertEUR() {
+    return amount.value * 0.012;
+}
